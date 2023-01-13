14963dc919bfc706d65eac773235e3a5943250ab
Frenchie AP <anthony.pepinoi1@gmail.com>
2023-01-13 12:30:36 -0800

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useParams, Link } from "react-router-dom";
+
+const Nav = (props) => {
+    return (
+        <div>
+            <div className="nav">
+                <h3><a href="/" className="navtab">Antoine Pepino - Full Stack Developer</a></h3>
+                <div className="navtabs">
+                    <h4><a href="/about" className="navtab">About</a></h4>
+                    <h4><a href="/projects" className="navtab">Projects</a></h4>
+                    <h4><a href="/resume" className="navtab">Resume</a></h4>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Nav;
